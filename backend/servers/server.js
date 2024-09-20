afd6cf9646386b6532ef6aecf6e4d82f6b99375f
szczun <szcisek64@gmail.com>
2024-09-20 20:24:52 +0200

    heroku deploy

---
+++ backend/servers/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "siemanko",
+    });
+});
+
+app.listen(process.env.PORT || 4003);
