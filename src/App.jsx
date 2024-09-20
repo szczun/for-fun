ea86c2c35ae944c5b9568d647e5c21462296244a
szczun <szcisek64@gmail.com>
2024-09-20 21:06:46 +0200

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
+import app from "../backend/servers/server";
 const App = () => {
     return (
         <div>Siemanko</div>
